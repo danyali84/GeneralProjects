e19a86db45a254283c696bd3c807a2f191ef44b1
danyali84 <lixdanya84@gmail.com>
2024-06-03 22:51:36 -0400

    bird physics done. started the game over behavior

--- flappybird/main.py
+++ flappybird/main.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 pygame.init()
 
 clock = pygame.time.Clock()
-fps = 30
+fps = 60
 
 screen_width = 710
 screen_height = 750
@@ -16,10 +16,45 @@ pygame.display.set_caption('Flappy Bird') #title
 #define game variables:
 ground_scroll = 0
 scroll_speed = 4
+flying = False
+game_over = False
 
 #load images
 background = pygame.image.load('images/flappybg4.png')
 ground_img = pygame.image.load('images/ground.png')
+
+#uses pygame's class
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/bird1.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
+
+    def update(self):
+        if flying == True:
+            #gravity
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8 #caps at 8 velocity
+            if self.rect.bottom < 580: #if its above ground, itll keep dropping
+                self.rect.y += int(self.vel)
+
+        #jump
+        if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: #checks if left clicked
+            self.clicked = True
+            self.vel = -10
+        if pygame.mouse.get_pressed()[0] == 0 : #ensures that you cant just hold jump
+            self.clicked = False
+
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
 
 run = True
 while run:
@@ -29,17 +64,29 @@ while run:
     #puts the image as the background
     screen.blit(background, (0,0))
 
-    #draw and scroll the ground
-    screen.blit(ground_img, (ground_scroll, 580))
-    ground_scroll -= scroll_speed #moves ground
+    #inserts bird using built in function "draw"
+    bird_group.draw(screen)
+    bird_group.update()
 
-    #restart image (puts it back in start pos) so it looks like it continuously scrolls
-    if abs(ground_scroll) > 59:
-        ground_scroll = 0
+    #draw ground
+    screen.blit(ground_img, (ground_scroll, 580))
+
+    #check if bird has hit ground
+    if flappy.rect.bottom > 580:
+        game_over = True
+        flying = False
+
+    if game_over == False:
+        ground_scroll -= scroll_speed #moves ground
+        #restart image (puts it back in start pos) so it looks like it continuously scrolls
+        if abs(ground_scroll) > 59:
+            ground_scroll = 0
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False #will stop running the program
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
 
     #update() ensures that the game actually updates itself after each iteration
     pygame.display.update()
