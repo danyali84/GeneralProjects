368d6ee86a7dfe2ef5d07f8e293e475c553d8618
danyali84 <lixdanya84@gmail.com>
2024-06-21 18:01:40 -0400

    restart done

--- flappybird/main.py
+++ flappybird/main.py
@@ -37,10 +37,20 @@ pass_pipe = False
 background = pygame.image.load('images/flappybg4.png')
 ground_img = pygame.image.load('images/ground.png')
 
+#RESTART BUTTON
+button_image = pygame.image.load("images/restart.png")
+
 
 def draw_text(text, font, text_color, x, y):
     img = font.render(text, True, text_color)
     screen.blit(img, (x, y))
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height/ 2)
+    score = 0
+    return score
 
 #uses pygame's class
 class Bird(pygame.sprite.Sprite):
@@ -87,6 +97,27 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill() #deletes the pipe to save storage
 
+#RESTART BUTTON
+class Button():
+    def __init__(self, x , y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        action = False
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+
+        #check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
 
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
@@ -94,6 +125,7 @@ pipe_group = pygame.sprite.Group()
 flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
 
+button = Button(screen_width//2 - 50, screen_height//2 - 100, button_image)
 
 run = True
 while run:
@@ -152,6 +184,20 @@ while run:
             ground_scroll = 0
 
         pipe_group.update()
+
+    #check for game over and reset
+    #THIS IS WITH BUTTON
+    #uncomment this if you want the button, and comment the without button section
+    # if game_over == True:
+    #     if button.draw() == True:
+    #         game_over = False
+    #         score = reset_game()
+
+    #WIHTOUT BUTTON
+    if game_over == True:
+        game_over = False
+        score = reset_game()
+            
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
